c15012256d3976a2970d6181f5cf97154e14c730
Bert Barabas <17627242+bertbarabas@users.noreply.github.com>
2025-05-06 19:39:37 -0500

    Updated to show:
    1. Styles that stop functioning with @rx.memo
    2. The memo crashing if you don't first make sure the state is hydrated.
    3. The double loading of @emotion/react
    4. The failure of set_focus to find the id specified in the react-select component.

--- react_select_demo/react_select_demo/react_select_demo.py
+++ react_select_demo/react_select_demo/react_select_demo.py
@@ -67,43 +67,74 @@ def index() -> rx.Component:
             rx.center(
                 rx.heading("React Select with", size="5"),
                 rx.heading("Reflex Custom Component Wrapping", size="5"),
-                spacing="0",
+                dark_mode_toggle(),
+                spacing="2",
                 direction="column",
             ),
-            dark_mode_toggle(),
+            rx.cond(
+                rx.State.is_hydrated,  # without this rx.memo causes exception
+                form_with_selections(),
+            ),
             direction="column",
-            spacing="3",
+            spacing="6",
             width="100%",
             padding_top="2em",
         ),
-        react_select(
-            name="test_dropdown_for_form",
-            placeholder="Select street address",
-            options=StreetState.streets,
-            on_input_change=lambda info: StreetState.on_input_change(info),
-            no_options_message=rx.vars.FunctionStringVar(
-                """(select_object) => {
+    )
+
+
+@rx.memo  # comment memo out to get styles to work for dark mode
+def form_with_selections() -> rx.Component:
+    return rx.form.root(
+        rx.vstack(
+            rx.form.field(
+                rx.form.label(
+                    "Street Address",
+                    size="4",
+                    weight="medium",
+                ),
+                react_select(
+                    id="selected_street_id",
+                    name="selected_street",
+                    placeholder="Select street address",
+                    options=StreetState.streets,
+                    on_input_change=StreetState.on_input_change,
+                    no_options_message=rx.vars.FunctionStringVar(
+                        """(select_object) => {
                         return select_object.inputValue.length < 2 ?
                             "Provide at least the first 2 digits of your house number \
                              to get a list of street addresses to choose from" :
                             `No street addresses start with: ${select_object.inputValue}`;}"""
+                    ),
+                    width="20em",
+                    on_mount=rx.set_focus("selected_street_id"),    # never finds the id although you can see it is there on the element
+                ),
+                key="selected_street_key",
             ),
-            width="20em",
-        ),
-        react_select(
-            name="test_dropdown_for_form",
-            placeholder="Select foods desired ",
-            is_multi=True,
-            close_menu_on_select=False,
-            options=food_options,
-            no_options_message=rx.vars.FunctionStringVar(
-                "(select_object) => `No options include: ${select_object.inputValue}`"
+            rx.form.field(
+                rx.form.label(
+                    "Desired foods",
+                    size="4",
+                    weight="medium",
+                ),
+                react_select(
+                    id="desired_foods_id",
+                    name="desired_foods",
+                    placeholder="Select foods desired ",
+                    is_multi=True,
+                    close_menu_on_select=False,
+                    options=food_options,
+                    no_options_message=rx.vars.FunctionStringVar(
+                        "(select_object) => `No options include: ${select_object.inputValue}`"
+                    ),
+                    width="20em",
+                ),
+                key="desired_foods_key",
             ),
-            width="20em",
+            align="center",
+            direction="column",
+            spacing="7",
         ),
-        align="center",
-        spacing="7",
-        height="100vh",
     )
 
 
